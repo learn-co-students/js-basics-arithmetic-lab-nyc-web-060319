43380ddd4f3b2a0b2833bc398777491f3ec5c543
CindyChenx <chen_cx0128@hotmail.com>
2019-07-13 23:48:05 -0400

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const var1 = 31;
 const var2 = 2;
 const multiply = var1 * var2;
 
-const random = Math.random()*10 + 1;
+const random = Math.floor(random()*10) + 1;
 
 const var3 = 16;
 const var4 = 6;
